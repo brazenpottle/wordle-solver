45ce13c8b69712262b33aabcdab3e9e0179f02be
chickenflyshigh <jamesli12345678908@gmail.com>
2022-02-09 00:55:50 +1100

    repeated letters

--- entropy.py
+++ entropy.py
@@ -15,7 +15,7 @@ def split_word_list(the_list):
     return split_list
 
 allowable_guesses_split = split_word_list(allowable_guesses)
-
+'''
 def generate_permutations(word): # for 
     permutations = []
     word_split = list(word)
@@ -39,6 +39,58 @@ def generate_permutations(word): # for
                     index_permutation[i] = b
         permutations.append(index_permutation)
     return permutations
+'''
+def generate_permutations(word): 
+    final_permutations = []
+    permutations = []
+    word_split = list(word)
+    counter = -1
+    length = len(word_split)
+    word_indexing = []
+
+    for idx, letter in enumerate(word_split):
+        if letter not in word_split[:idx]:
+            counter +=1
+            word_indexing.append(counter)
+        else:
+            word_indexing.append(word_split.index(letter))
+    dupe_dict = {}
+    for i, j in enumerate(word_indexing):
+        dupe_dict[j] = dupe_dict.get(j,[] ) + [i]
+    dupe_dict_2 = {}
+    for key, value in dupe_dict.items():
+        if len(value) > 1:
+            dupe_dict_2[key] = value
+    dupe_1 = [0]
+    dupe_2 = [1,2]
+    no_dupe_0 = [0,1,2]
+    is_dupe = [1,2] # choice to use either dupe_1 or dupe_2
+    no_of_dupes = len(dupe_dict_2)
+    variations = []       
+    if no_of_dupes > 0:
+        dupe_permutations = itertools.product(is_dupe, repeat = no_of_dupes)
+        for permutation in dupe_permutations:
+            counter = 0
+            variation = [0]*len(word)
+            for key, values in dupe_dict_2.items():
+                for index in values:
+                    variation[index] = permutation[counter]
+                counter += 1        
+            variations.append(variation)
+    else:
+        variations.append([0]*len(word))
+    for variation in variations:
+        for index, choice in enumerate(variation):
+            if choice == 0:
+                variation[index] = no_dupe_0
+            elif choice == 1:
+                variation[index] = dupe_1
+            else:
+                variation[index] = dupe_2
+        permutations.append(variation)
+    for permutation in permutations:
+        final_permutations.extend(list(itertools.product(*permutation)))
+    return final_permutations
 
 def possible_words_number_s(array, word,word_list_split):
     total_no = 0
@@ -107,28 +159,30 @@ def entropy(word, word_list):
 
 if __name__ == "__main__":
     '''
+    def entropy_2(p):
+        return entropy(p, allowable_guesses)
     start = time.time()
     with Pool(os.cpu_count()-1) as p:
-        data = p.map(entropy, allowable_guesses)
-    np.savetxt("entropyrest.csv", data, delimiter = ',', fmt = '% s')
+        data = p.map(entropy_2, allowable_guesses)
+    np.savetxt("entropyrepeated.csv", data, delimiter = ',', fmt = '% s')
     end = time.time()
     print(end-start)
     '''
 
-word = input('Enter the five letter word you put: ') 
-correctness_array = list(map(int,list(input('Enter the correctness array: '))))
-word_list = possible_words(correctness_array,word, allowable_guesses_split)
-word_list_split = split_word_list(word_list)
-new_list = []
-for wd in word_list:
-    new_list.append(entropy(wd,word_list))
-print(pd.DataFrame(new_list).sort_values(1, ascending=False))
-while 1:
-    word = input('Enter a five letter word you put: ')
+    word = input('Enter the five letter word you put: ') 
     correctness_array = list(map(int,list(input('Enter the correctness array: '))))
-    word_list = possible_words(correctness_array, word, word_list_split)
+    word_list = possible_words(correctness_array,word, allowable_guesses_split)
     word_list_split = split_word_list(word_list)
     new_list = []
     for wd in word_list:
         new_list.append(entropy(wd,word_list))
-    print(pd.DataFrame(new_list).sort_values(1, ascending=False))
+    print(pd.DataFrame(new_list).sort_values(1, ascending=True))
+    while 1:
+        word = input('Enter a five letter word you put: ')
+        correctness_array = list(map(int,list(input('Enter the correctness array: '))))
+        word_list = possible_words(correctness_array, word, word_list_split)
+        word_list_split = split_word_list(word_list)
+        new_list = []
+        for wd in word_list:
+            new_list.append(entropy(wd,word_list))
+        print(pd.DataFrame(new_list).sort_values(1, ascending=True))
